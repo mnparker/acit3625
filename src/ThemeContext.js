3d4d3cc01e2ff857d695e6c53ceb33d2a51ebf7c
Mike Parker <michaelnparker@gmail.com>
2020-11-25 15:17:52 -0800

    assignment10

--- src/ThemeContext.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-
-//first element in array is current theme mode
-//second is function that would be used to update theme
-const ThemeContext = React.createContext(["light", () => {}]);
-export default ThemeContext;
-
